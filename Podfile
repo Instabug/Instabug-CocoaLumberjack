6b40bad8d6175edc6e9973f7ed1540a69481c841
Artur Grigor <arturgrigor@gmail.com>
2017-03-21 14:13:11 +0200

    - Added framework and test schemes for each supported platform. - Made the schemes Shared for Carthage support. - Embedded the Pods in the repo for Carthage build support. - Bumped the pod version.

---
+++ Podfile
@@ -0,0 +1,8 @@
+abstract_target 'Framework' do
+  pod 'Instabug', '>= 6.2'
+  pod 'CocoaLumberjack'
+
+  target 'Instabug-CocoaLumberjack iOS' do
+    platform :ios, '8.0'
+  end
+end
---
+++ Pods/Target Support Files/Pods-Framework-Instabug-CocoaLumberjack iOS/Pods-Framework-Instabug-CocoaLumberjack iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Framework_Instabug_CocoaLumberjack_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_Framework_Instabug_CocoaLumberjack_iOS
+@end
